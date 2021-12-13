c5624a30cf6cfc617dad50ebf73dea729130aff6
IslandMoney <olgermile@yahoo.com>
2021-12-13 03:40:45 +0100

    added bot is ready code

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require('discord.js')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.login(process.env.BOT_TOKEN);
+client.on("ready", () => {
+    console.log("Bot is ready")
+})
 
 client.on("messageCreate", msg => {
     if (msg.content.toLowerCase().trim() === "valerante") {
     msg.reply("valarante child game.... look to cartoon grapfix to make kid player happy like children show.. valarante cartoon world with rainbow unlike counter strike with dark corridorr and raelistic gun.. valarante like playhouse. valarant playor run from csgo fear of dark world and realism so need child game to relax")
     }
 })
+
+client.login(process.env.BOT_TOKEN)
