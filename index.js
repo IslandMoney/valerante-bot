e19fc7bad936b7a3134d0b46dc3c8366e36aed4f
IslandMoney <olgermile@yahoo.com>
2021-12-13 03:33:31 +0100

    slight change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.login(process.env.BOT_TOKEN);
 
