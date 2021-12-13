bc528e16a5ae51f594f4cec3e0274509e7936460
IslandMoney <olgermile@yahoo.com>
2021-12-13 03:42:40 +0100

    added some logging

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", msg => {
+    console.log(msg.content.toLowerCase().trim());
     if (msg.content.toLowerCase().trim() === "valerante") {
-    msg.reply("valarante child game.... look to cartoon grapfix to make kid player happy like children show.. valarante cartoon world with rainbow unlike counter strike with dark corridorr and raelistic gun.. valarante like playhouse. valarant playor run from csgo fear of dark world and realism so need child game to relax")
+        msg.reply("valarante child game.... look to cartoon grapfix to make kid player happy like children show.. valarante cartoon world with rainbow unlike counter strike with dark corridorr and raelistic gun.. valarante like playhouse. valarant playor run from csgo fear of dark world and realism so need child game to relax")
     }
 })
 
